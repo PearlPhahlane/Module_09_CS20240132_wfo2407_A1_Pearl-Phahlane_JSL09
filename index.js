949b73aa7857a4d5765aa0de36a2fb638ea92d7e
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-09-27 10:54:26 +0200

    fix first line link and add nature after query

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=hjfhgfhj")
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(res => res.json())
     .then(data => {
         document.body.style.backgroundImage = `url(${data.urls.regular})`
